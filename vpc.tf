b82c8ed791156ea8cfd01c7e802fad61f56fc65d
shylaja-devops <shylaja_devops@gmail.com>
2021-12-29 13:09:39 +0000

    tags attached for vpc

--- vpc.tf
+++ vpc.tf
@@ -2,7 +2,7 @@ resource "aws_vpc" "javahome_vpc"{
   cidr_block = var.vpc_cidr
   instance_tenancy = var.tenancy
   tags = {
-    NAme = "${terraform.workspace}-javahome-vpc"
+    Name = "${terraform.workspace}-javahome-vpc"
   }
 }
 
